240872a01a315bb04d6d76270c5104415c79010d
garbageman <dnagle@umd.edu>
2016-05-24 21:55:41 -0400

    Made changes to guessing game

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
+/**
+* This is the main function of the program, it will execute when the compiled code is run.
+*/
 fn main() {
-    println!("Hello, world!");
+    /* Standard println function */
+    println!("Guess the number!");
+
+    println!("Please input your guess");
+
+    /* Creating a mutable object for storing the input */
+    let mut guess = String::new();
+
+    /* Using std in to read the next line of input */
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    /* Finally output the line with the guess */
+    println!("You guessed {}", guess);
 }
